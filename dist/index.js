089e25d960285fc10638d8ae0b2dc564919f6c41
Rafal F <functionalweasel@gmail.com>
2022-05-06 15:30:28 +0200

    feat: add post'/create/' for add Doc and get'/' for getDocs

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { addDoc, getDocs } = require('firebase/firestore/lite');
 
 const users = require('./configDB');
 
@@ -8,10 +9,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', async (req, res) => {
+  let usersDocs = [];
+
+  await getDocs(users).then((snapshot) => {
+    snapshot.forEach((doc) => {
+      usersDocs.push({ id: doc.id, ...doc.data() });
+    });
+  });
+  res.send(usersDocs);
+});
+
 app.post('/create', async (req, res) => {
   const data = req.body;
-  console.log('Body: ', data);
+  await addDoc(users, data);
   res.send({ message: 'User added' });
 });
 
-app.listen(4000, console.log('app listening on port4000'));
+app.listen(4000, console.log('app listening on port 4000'));
